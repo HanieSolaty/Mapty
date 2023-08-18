b8aeb78d2463eace834bf3bf41c5e0aacc934df9
HanieSolaty <88339589+HanieSolaty@users.noreply.github.com>
2023-08-18 13:23:58 +0330

    Adding modal window to the project

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Modal window
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const modalHeader = document.querySelector('.modal__header');
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//Modal window functions
+const openModal = function (str) {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  modalHeader.textContent = str;
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
